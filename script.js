5b2f2fcd50174fc8f53991de230da3e418fcfd04
santskimi-oss <santskimi@gmail.com>
2026-08-27 22:01:21 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
- const botaoTeste = document.querySelector('#testarProjeto');
- const statusProjeto = document.querySelector('#statusProjeto');
-
- botaoTeste.addEventListener('click', () => {
-    statusProjeto.textContent = 'Projeto verificado: HTML, CSS e JavaScript estão conectados.';
-    statusProjeto.classList.add('sucesso');
-    botaoTeste.textContent = 'Ambiente verificado';
- });
